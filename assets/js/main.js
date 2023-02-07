cf0b42a0bcf6e63f38216eeaecfe13f843247daa
analuisadev <analuisavidal89@gmail.com>
2023-02-07 15:04:37 -0300

    feat: calculator starter logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,37 @@ const btnClear = document.querySelector('.btn-clear')
 const result = document.querySelector('.calc-result')
 const infoCalc = document.querySelector('.info-calc')
 
+// Remove Result
 const clearResult = () => {
-    result.innerHTML = '0'
+    result.innerHTML = ''
 }
 
+// Remove All 
 const removeCalc = () => {
-   result.innerHTML = '0'
+   result.innerHTML = ''
    infoCalc.innerHTML = ''
+}
+
+// Show Digit Numbers and Expressions
+const showNumber = (num) => {
+    const number = document.querySelector('.calc-result').innerHTML
+    document.querySelector('.calc-result').innerHTML = number + num
+}
+
+
+// Calculator Lógic
+const calculate = () => {
+    const expressionCalc = document.querySelector('.calc-result').innerHTML
+    document.querySelector('.info-calc').innerHTML = expressionCalc
+    
+    if (!expressionCalc) {
+        alert('Informe valores válidos!')
+    }
+
+    try {
+        const result = eval(expressionCalc)
+        document.querySelector('.calc-result').innerHTML = result
+      } catch (error) {
+        document.querySelector('.calc-result').innerHTML = 'Error'
+      }
 }
